20ce857acd6fd940ebb00317ac7740de207906f1
Irina Saveleva <fomina_88@mail.ru>
2021-12-23 15:37:50 +0400

    add tests

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -43,4 +43,69 @@ public class CardOrderTest {
         String message = driver.findElement(cssSelector("[data-test-id='order-success']")).getText();
         Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", message.strip());
     }
+    @Test
+    void shouldTestV2() {
+        driver.get("http://localhost:9999/");
+        WebElement form = driver.findElement(cssSelector("form"));
+        driver.findElement(cssSelector("[data-test-id='phone'] input")).sendKeys("+79258745098");
+        driver.findElement(cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input_invalid .input__sub")).getText();
+        Assertions.assertEquals("Поле обязательно для заполнения", message.strip());
+    }
+
+    @Test
+    void shouldTestV3() {
+        driver.get("http://localhost:9999/");
+        WebElement form = driver.findElement(cssSelector("form"));
+        driver.findElement(cssSelector("[data-test-id='name'] input")).sendKeys("Фомина Ирина");
+        driver.findElement(cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input_invalid .input__sub")).getText();
+        Assertions.assertEquals("Поле обязательно для заполнения", message.strip());
+    }
+
+    @Test
+    void shouldTestV4() {
+        driver.get("http://localhost:9999/");
+        WebElement form = driver.findElement(cssSelector("form"));
+        driver.findElement(cssSelector("[data-test-id='name'] input")).sendKeys("Фомина Ирина");
+        driver.findElement(cssSelector("[data-test-id='phone'] input")).sendKeys("+79258745098");
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input_invalid")).getText();
+        Assertions.assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", message.strip());
+    }
+
+    @Test
+    void shouldTestV5() {
+        driver.get("http://localhost:9999/");
+        WebElement form = driver.findElement(cssSelector("form"));
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input__sub")).getText();
+        Assertions.assertEquals("Поле обязательно для заполнения", message.strip());
+    }
+
+    @Test
+    void shouldTestV6() {
+        driver.get("http://localhost:9999/");
+        WebElement form = driver.findElement(cssSelector("form"));
+        driver.findElement(cssSelector("[data-test-id='name'] input")).sendKeys("Fomina Irina");
+        driver.findElement(cssSelector("[data-test-id='phone'] input")).sendKeys("+79258745098");
+        driver.findElement(cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input_invalid .input__sub")).getText();
+        Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", message.strip());
+    }
+
+    @Test
+    void shouldTestV7() {
+        driver.get("http://localhost:9999/");
+        WebElement form = driver.findElement(cssSelector("form"));
+        driver.findElement(cssSelector("[data-test-id='name'] input")).sendKeys("Фомина Ирина");
+        driver.findElement(cssSelector("[data-test-id='phone'] input")).sendKeys("79258745098");
+        driver.findElement(cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input_invalid .input__sub")).getText();
+        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", message.strip());
+    }
 }
